b27b2b440ec3a26e170f3100d49e21108925b489
Konstantin Kuzmin <kosax22@gmail.com>
2023-10-22 13:54:29 +0200

    Initial commit

---
+++ imports.tf
@@ -0,0 +1,50 @@
+##################################################################################
+# IMPORTS
+##################################################################################
+
+# AFTER SUCCESSFUL IMPORT THIS FILE CAN BE DELETED
+
+import {
+  to = module.main.aws_vpc.this[0]
+  id = "vpc-02df83e2e659a9fc9" #VPC
+}
+
+import {
+  to = module.main.aws_subnet.public[0]
+  id = "subnet-0686d7c79343f3c79" #PublicSubnet1
+}
+
+import {
+  to = module.main.aws_subnet.public[1]
+  id = "subnet-0599bbdd295b09fa0" #PublicSubnet2
+}
+
+import {
+  to = module.main.aws_internet_gateway.this[0]
+  id = "igw-0bf411542dfae0e7f" #InternetGateway
+}
+
+import {
+  to = module.main.aws_route.public_internet_gateway[0]
+  id = "rtb-0ff36f500260ad5be_0.0.0.0/0" #DefaultPublicRoute
+}
+
+import {
+  to = module.main.aws_route_table.public[0]
+  id = "rtb-0ff36f500260ad5be" #PublicRouteTable
+}
+
+import {
+  to = module.main.aws_route_table_association.public[0]
+  id = "subnet-0686d7c79343f3c79/rtb-0ff36f500260ad5be" #PublicSubnet1/PublicRouteTable
+}
+
+import {
+  to = module.main.aws_route_table_association.public[1]
+  id = "subnet-0599bbdd295b09fa0/rtb-0ff36f500260ad5be" #PublicSubnet2/PublicRouteTable
+}
+
+import {
+  to = aws_security_group.ingress
+  id = "sg-005ec93646d55352c" #NoIngressSecurityGroup
+}
